6e2aa0a354cb87b658f321e7ba38e5d01d590162
sivaprabha679-gif <sivaprabha679@gmail.com>
2026-03-13 21:57:37 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import cocotb
-from cocotb.triggers import Timer
-
-@cocotb.test()
-async def test_project(dut):
-    dut._log.info("Starting dummy test...")
-    await Timer(1, units="ns")
-    dut._log.info("Test passed!")
